74556511388f0c0055f5bb41679fcb5c5e735b25
Luke Sneeringer <luke@sneeringer.com>
2014-05-21 15:12:46 -0500

    Allow dummy db backend without geos installed.

--- django_pg/utils/gis.py
+++ django_pg/utils/gis.py
@@ -3,7 +3,32 @@ from django.db import connections
 from django.db.utils import DEFAULT_DB_ALIAS
 
 
-gis_backend = any((
-    '.gis.' in connections[DEFAULT_DB_ALIAS].__module__,
-    '.dummy.' in connections[DEFAULT_DB_ALIAS].__module__,
-))
+def _detect_gis_backend():
+    """Determine whether or not a GIS backend is currently in use,
+    to allow for divergent behavior elsewhere.
+    """
+    # If the connection is from `django.contrib.gis`, then we know that this
+    # is a GIS backend.
+    if '.gis.' in connections[DEFAULT_DB_ALIAS].__module__:
+        return True
+
+    # Annoying case: If we're using a dummy backend (the most likely reason
+    # being because, for testing, the database is mocked out), we need to
+    # determine GIS presence or absence in such a way that will work on
+    # the system.
+    #
+    # We have to approximate this; essentially, return True if geos is
+    # installed, and False otherwise.  We can determine this by trying to
+    # import GEOSException.
+    if '.dummy.' in connections[DEFAULT_DB_ALIAS].__module__:  # pragma: no cover
+        try:
+            from django.contrib.gis.geos import GEOSException
+            return True
+        except ImportError:
+            return False
+
+    # Okay, there's no GIS backend in use.
+    return False
+
+
+gis_backend = _detect_gis_backend()
